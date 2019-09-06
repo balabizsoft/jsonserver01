2c6f45d2cd82bdf34e630bd4759fa1d649e4b214
bala <bala.bizsoft@gmail.com>
2019-09-06 15:52:38 +0800

    create db.json and write events

---
+++ js/employee.js
@@ -0,0 +1,19 @@
+$(function(){
+
+    $('#btnInsert').click(function(){
+        console.log('Insert');
+    });
+
+    $('#btnUpdate').click(function(){
+        console.log('Update');
+    });
+
+    $('#btnDelete').click(function(){
+        console.log('Delete');
+    });
+
+    $('#btnSearch').click(function(){
+        console.log('Search');
+    });
+
+});
