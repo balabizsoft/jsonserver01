b31e7b1525584eb73fa748bf2bc40b427e4c76f5
bala <bala.bizsoft@gmail.com>
2019-09-06 16:09:45 +0800

    Insert Code Done

--- js/employee.js
+++ js/employee.js
@@ -2,6 +2,27 @@ $(function(){
 
     $('#btnInsert').click(function(){
         console.log('Insert');
+
+        var urlAPI = 'http://localhost:4000/Employees';
+        var dataReq = {
+            empCode : $('#empCode').val(),
+            empName : $('#empName').val(),
+            empMobile : $('#empMobile').val(),
+            empEMail : $('#empEMail').val(),
+        };
+
+        jQuery.ajax({
+            async: false,
+            type: "POST",
+            url: urlAPI,
+            data: dataReq,
+            dataType: "json",
+            success:function (data) {
+                alert("Inserted : " + data.id );
+                console.log(data);
+             }
+         });
+
     });
 
     $('#btnUpdate').click(function(){
